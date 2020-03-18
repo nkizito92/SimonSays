7dad12405d1553f441f8e3760b7d3cd5232eca6e
nkizito92 <nkizito57@gmail.com>
2020-03-18 16:04:23 -0500

    checking if fetch config

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -1,8 +1,12 @@
-alert("Hi");
 
-let div = document.createElement("div");
-
-div.innerHTML = "Hello there!";
-
-let body = document.querySelector("body");
-body.append(div)
+fetch("http://localhost:3000/users")
+    .then(res => res.json())
+    .then(user => {
+        let body = document.querySelector("body");
+        user.forEach(player => {
+            debugger
+            let div = document.createElement("div");
+            div.innerHTML = `${player.name} and ${player.games.length}`
+            body.appendChild(div)
+        })
+    })
