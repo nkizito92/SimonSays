811ca5b871dce4a9ae2c24c351bc69a9fe912686
nkizito92 <nkizito57@gmail.com>
2020-03-19 21:12:58 -0500

    adjusted CSS for the button, appended buttons

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -1,20 +1,31 @@
 const body = document.querySelector("body");
 const main = document.querySelector("main")
+const createUser = document.querySelector("#createUser")
+const startGame = document.querySelector("#startbtn")
+const keyButtons = document.querySelector("#keysbuttons")
 
-fetch("http://localhost:3000/games")
-    .then(res => res.json())
-    .then(game => {
-        let gm = document.querySelector("#gm")
-        let bestPlayer = gamehightScore(game)[0];
-        gm.innerHTML = `${bestPlayer.player}'s HightScore: ${bestPlayer.highscore}`
-    })
+function startsTheGame() {
+    fetch("http://localhost:3000/games")
+        .then(res => res.json())
+        .then(game => {
+            let gm = document.querySelector("#gm")
+            let bestPlayer = gamehightScore(game)[0];
+            gm.innerHTML = `${bestPlayer.player}'s HightScore: ${bestPlayer.highscore}`
+            for (let i = 0; i < 6; i++) {
+                let keyButton = document.createElement("div")
+                keyButton.className = "keybutton"
+                keyButton.innerHTML = "keys"
+                keyButtons.appendChild(keyButton)
+            }
 
-
-function startGame() {
-
+        })
+    // buttons appear 
+    // cpu clicks buttons and they each blink once
+    // you click the same buttons and pass the first level
+    // cpu click buttons and adds one more click to follow each level
 }
 
-
+startGame.addEventListener("click", startsTheGame)
 
 function hightScore(highscores) {
     return highscores.games.sort((a, b) => b.highscore - a.highscore)
@@ -46,3 +57,23 @@ fetch("http://localhost:3000/users")
         })
     })
 
+function submitUser(name) {
+    fetch("http://localhost:3000/users", {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name
+        })
+    })
+        .then(res => res.json())
+        .then(user => console.log(user))
+}
+
+let NewUser = document.querySelector("#name")
+createUser.addEventListener("click", (e) => {
+    submitUser(NewUser.value)
+    e.preventDefault()
+})
