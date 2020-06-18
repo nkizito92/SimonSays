20c0d72bb3b9c04f5ef1b0cd429ef10e10703e58
nkizito92 <nkizito57@gmail.com>
2020-06-17 21:25:54 -0500

    test 1

--- backEnd/db/schema.rb
+++ backEnd/db/schema.rb
@@ -10,11 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_173155) do
+ActiveRecord::Schema.define(version: 2020_04_20_161213) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "adminpack"
+  enable_extension "plpgsql"
 
   create_table "buttons", force: :cascade do |t|
     t.string "name"
     t.string "sound"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "chats", force: :cascade do |t|
+    t.string "message"
+    t.string "guest_id"
+    t.string "img"
+    t.integer "like"
+    t.integer "laugh"
+    t.integer "angry"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.string "img"
+    t.integer "chat_id"
+    t.integer "guest_id"
+    t.integer "like"
+    t.integer "laugh"
+    t.integer "angry"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -26,10 +53,17 @@ ActiveRecord::Schema.define(version: 2020_03_20_173155) do
     t.integer "user_id"
   end
 
+  create_table "guests", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "scored"
   end
 
 end
